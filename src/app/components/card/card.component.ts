69ee451ed055c0961c64f6917c15196bc7cce433
AKASH S P <akashsp12@outlook.com>
2023-12-10 20:32:26 +0530

    wokring on ncard

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
   standalone:true,
-  encapsulation: ViewEncapsulation.ShadowDom,
+  imports:[IonCard,IonCardContent,IonCardTitle,IonCardHeader,IonCardSubtitle]
 })
 export class CardComponent  implements OnInit {
 

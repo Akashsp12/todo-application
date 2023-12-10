91b087c4dd4044521a74210e9022051aa0ac72e8
AKASH S P <akashsp12@outlook.com>
2023-12-10 07:24:07 +0530

    working on card

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,21 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, ViewChild, ViewChildren, inject } from '@angular/core';
-import { RefresherCustomEvent, IonHeader, IonToolbar, IonTitle, IonContent, IonRefresher, IonRefresherContent, IonList, IonButton } from '@ionic/angular/standalone';
+import { RefresherCustomEvent, IonHeader, IonToolbar, IonTitle, IonContent, IonRefresher, IonRefresherContent, IonList, IonButton, IonLabel } from '@ionic/angular/standalone';
 
 import { DataService, Message } from '../services/data.service';
 import { HeaderComponent } from '../header/header.component';
 import { DatepickerComponent } from '../components/datepicker/datepicker.component';
 import { FeatherIconsModule } from '../feather-icons/feather-icons.module';
+import { CardComponent } from '../components/card/card.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonButton, IonContent, IonRefresher, IonRefresherContent, IonList, HeaderComponent, DatepickerComponent, FeatherIconsModule],
+  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonButton, IonContent, IonRefresher, IonRefresherContent,IonLabel, IonList, HeaderComponent, DatepickerComponent, FeatherIconsModule,CardComponent],
 })
 export class HomePage {
-  private data = inject(DataService);
+  // private data = inject(DataService);
 
   temp: number = 0
   finalresult: any[] = [];

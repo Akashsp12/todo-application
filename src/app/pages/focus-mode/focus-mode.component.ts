1160ae0480b545291de9b57522e54e0a06ff66fd
akashsp12 <akashsp12@outlook.com>
2023-12-15 10:21:51 +0530

    small changes

--- src/app/pages/focus-mode/focus-mode.component.ts
+++ src/app/pages/focus-mode/focus-mode.component.ts
@@ -90,15 +90,15 @@ export class FocusModeComponent implements OnInit, AfterViewInit {
       columns: [
         {
           name: 'hours',
-          options: hoursArray.map(h => ({ text: h.toString().padStart(2, '0'), value: h }))
+          options: hoursArray.map(h => ({ text: h.toString().padStart(2, '0')}))
         },
         {
           name: 'minutes',
-          options: minutesArray.map(m => ({ text: m.toString().padStart(2, '0'), value: m }))
+          options: minutesArray.map(m => ({ text: m.toString().padStart(2, '0')}))
         },
         {
           name: 'seconds',
-          options: secondsArray.map(s => ({ text: s.toString().padStart(2, '0'), value: s }))
+          options: secondsArray.map(s => ({ text: s.toString().padStart(2, '0')}))
         }
       ]
     };
@@ -151,11 +151,6 @@ export class FocusModeComponent implements OnInit, AfterViewInit {
         this.timing = `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`
         console.log(distance)
         this.dasharray = `${(502 * percentageDistance)/100}  502`;
-
-        // console.log(this.dasharray)
-        //  this.dasharray = `${((this.distance * 502) / 100) / 100} 502`;
-
-        // Accessing the circle element and setting the stroke dash array
         this.myCircle.nativeElement.setAttribute('stroke-dasharray', this.dasharray);
 
 

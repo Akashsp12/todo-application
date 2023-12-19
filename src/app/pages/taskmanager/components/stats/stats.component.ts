7f67e282a21d629d4fe797eafa9ee3a0d91b0030
akashsp12 <akashsp12@outlook.com>
2023-12-19 14:25:00 +0530

    working on categorys

--- src/app/pages/taskmanager/components/stats/stats.component.ts
+++ src/app/pages/taskmanager/components/stats/stats.component.ts
@@ -1,5 +1,5 @@
 import { NgFor } from '@angular/common';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { IonButton, IonCard, IonCardContent, IonContent, IonIcon, IonInput, IonItem, IonLabel, IonList } from '@ionic/angular/standalone';
 import { FeatherIconsModule } from 'src/app/feather-icons/feather-icons.module';
 
@@ -11,18 +11,29 @@ import { FeatherIconsModule } from 'src/app/feather-icons/feather-icons.module';
   imports: [IonContent, IonList, NgFor, IonItem, IonIcon, IonButton, IonInput, FeatherIconsModule, IonLabel, IonCard, IonCardContent]
 })
 export class StatsComponent implements OnInit {
-  @ViewChild('categoryList') categoryList: any
-  constructor() { }
+  @ViewChild('card') cardElement: ElementRef | undefined
+  constructor(private renderer: Renderer2) { }
   categoryArray: any = []
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
   addCategory(ev: any) {
-    let a = ev
-    if (ev.length > 1) {
-      this.categoryArray.push(a)
+    let inputResult = ev
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16)
+    if (this.cardElement) {
+      const para = this.renderer.createElement('p');
+      this.renderer.setProperty(para, 'innerHTML', inputResult)
+      this.renderer.addClass(para, 'categorys')
+      this.renderer.setStyle(para, 'color', `#${randomColor}`)
+      this.renderer.setStyle(para, 'font-weight', 400)
+      this.renderer.setStyle(para, 'background-color', this.hexToRGBA(randomColor, 0.2))
+      this.renderer.appendChild(this.cardElement.nativeElement, para);
     }
-  }
 
+  }
+  hexToRGBA(hex: string, alpha: number): string {
+    const r = parseInt(hex.substring(1, 3), 16);
+    const g = parseInt(hex.substring(3, 5), 16);
+    const b = parseInt(hex.substring(5, 7), 16);
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+  }
 
 }

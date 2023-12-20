30b8de7b51f03015255182cbbb4aa3dbb5f02054
akashsp12 <akashsp12@outlook.com>
2023-12-20 16:26:18 +0530

    working on notification

--- src/app/pages/notifications/notifications.component.ts
+++ src/app/pages/notifications/notifications.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { IonButton, IonIcon, IonItem, IonLabel, IonList, IonListHeader, IonNote, IonText } from '@ionic/angular/standalone';
+import { FeatherIconsModule } from 'src/app/feather-icons/feather-icons.module';
 
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
   styleUrls: ['./notifications.component.scss'],
   standalone:true,
-  imports:[]
+  imports:[IonList,IonListHeader,IonButton,IonItem,IonLabel,IonText,IonNote,IonIcon,FeatherIconsModule]
 })
 export class NotificationsComponent  implements OnInit {
 
